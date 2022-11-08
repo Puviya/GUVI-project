a17b0e4933d93346b61716d4e9bd62e54253aee7
Puviya <87022532+Puviya@users.noreply.github.com>
2022-11-08 10:34:54 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     function checkemail(email){
         $.ajax({
             method:'POST',
-            url:'../php/login.php',
+            url:'./../php/login.php',
             data:'email='+email,
             success:function(data){
                 $("#email-availability-status").html(data);
@@ -22,7 +22,7 @@ $(document).ready(function(){
     function checkpassword(password,email){
         $.ajax({
             method:'POST',
-            url:'../php/login.php',
+            url:'./../php/login.php',
             data:{'password':password,'email':email},
             success:function(data){
                 $("#password-status").html(data);
@@ -34,6 +34,6 @@ $(document).ready(function(){
 function login_status(){
     if($("#email-availability-status").text()=="Success" && $("#password-status").text()=="Success"){
         $("#response-message").html('<span class="text-success">Registered successfully</span>');
-        window.location.href="../profile.html";
+        window.location.href="./../profile.html";
     }
 }
